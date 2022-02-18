60041e9b505258505f866b3292c36cfe1ee3630d
elkilani <elkarous.elkilani@gmail.com>
2022-02-18 11:38:08 +0100

    sonar sprint 1

---
+++ src/main/java/lb/spring/repositories/FileDBRepository.java
@@ -0,0 +1,12 @@
+package lb.spring.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import lb.spring.entities.FileDB;
+
+@Repository
+public interface FileDBRepository extends JpaRepository<FileDB, String> {
+
+}
---
+++ src/main/java/lb/spring/services/FileStorageService.java
@@ -0,0 +1,26 @@
+package lb.spring.services;
+
+import java.io.IOException;
+import java.util.Objects;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.util.StringUtils;
+import lb.spring.entities.FileDB;
+import lb.spring.repositories.FileDBRepository;
+
+@Service
+public class FileStorageService {
+  @Autowired
+ FileDBRepository fileDBRepository;
+
+    public FileDB store(MultipartFile file) throws IOException {
+        String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+        FileDB fileDb = new FileDB(fileName, file.getContentType(), file.getBytes());
+        return fileDBRepository.save(fileDb);
+    }
+
+
+}
